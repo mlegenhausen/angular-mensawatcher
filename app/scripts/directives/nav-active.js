b839f74f2af5a50c9c609319e70b5f15e2bc63e7
Malte Legenhausen <mlegenhausen@gmail.com>
2013-02-16 21:40:25 +0100

    nav-active directive added

---
+++ app/scripts/directives/nav-active.js
@@ -0,0 +1,12 @@
+angularFoodwatcherApp.directive('navActive', [
+	'$rootScope', '$location',
+	function($rootScope, $location) {
+		return {
+			link: function(scope, element, attrs) {
+				$rootScope.$on('$routeChangeSuccess', function() {
+					element.toggleClass('active', attrs.navActive === $location.path());
+				});
+			}
+		};
+	}
+]);
