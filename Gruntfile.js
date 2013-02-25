06d2b89aa6b4741f5128e248acf45123e53d5e98
Malte Legenhausen <mlegenhausen@gmail.com>
2013-02-25 21:39:00 +0100

    Fixed bootstrap glypin paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,6 +30,7 @@ module.exports = function( grunt ) {
       dist: {
         // http://compass-style.org/help/tutorials/configuration-reference/#configuration-properties
         options: {
+          config: 'compass.rb',
           css_dir: 'temp/styles',
           sass_dir: 'app/styles',
           images_dir: 'app/images',
